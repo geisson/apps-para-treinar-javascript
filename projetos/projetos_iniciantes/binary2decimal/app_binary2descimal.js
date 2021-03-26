566b273ad3e3dbe7e19694d06ab04cbdd206543a
Geisson Iuri <geissoniuri@gmail.com>
2021-03-26 13:42:12 -0300

    desenvolvendo funcionalidades

--- projetos/projetos_iniciantes/binary2decimal/app_binary2descimal.js
+++ projetos/projetos_iniciantes/binary2decimal/app_binary2descimal.js
@@ -1,14 +1,21 @@
 const inputBinaryNumber = document.querySelector('#binary-number');
 const inputDecimalNumber = document.querySelector('#decimal-number');
 const invalidFeedback = document.querySelector('[data-js=invalid-feedback]');
+const invalidNumber = document.querySelector(
+  '[data-js=display-invalid-number]',
+);
 
 inputBinaryNumber.addEventListener('input', (event) => {
   const valueInputBinaryNumber = event.target.value;
-  const lastDigitEntered = valueInputBinaryNumber.substr(-1, 1);
+  const lastDigitEntered = +valueInputBinaryNumber.substr(-1, 1);
   const valueDecimalNumber = parseInt(valueInputBinaryNumber, 2);
 
-  console.log(typeof lastDigitEntered);
-  console.log(lastDigitEntered);
+  if (lastDigitEntered !== 0 && lastDigitEntered !== 1) {
+    inputBinaryNumber.classList.add('is-invalid');
+    invalidNumber.innerHTML = lastDigitEntered;
+    return;
+  }
 
+  inputBinaryNumber.classList.remove('is-invalid');
   inputDecimalNumber.value = valueDecimalNumber;
 });
