7110670c107c8a3e67435ab9272041035b9cc502
Geisson Iuri <geissoniuri@gmail.com>
2021-03-26 23:13:53 -0300

    código refatorado

--- projetos/projetos_iniciantes/binary2decimal/app_binary2descimal.js
+++ projetos/projetos_iniciantes/binary2decimal/app_binary2descimal.js
@@ -4,21 +4,32 @@ const invalidNumber = document.querySelector(
   '[data-js=display-invalid-number]',
 );
 
-inputBinaryNumber.addEventListener('input', (event) => {
+const getLastCharacterTyped = (value) => +value.substr(-1, 1);
+
+const convertToDecimalNumber = (value) => parseInt(value, 2);
+
+const validateValueEntry = (value, regex) => regex.test(value);
+
+const processBinaryToDecimal = (event) => {
   const valueInputBinaryNumber = event.target.value;
-  const lastDigitEntered = +valueInputBinaryNumber.substr(-1, 1);
-  const valueDecimalNumber = parseInt(valueInputBinaryNumber, 2);
+  const lastDigitEntered = getLastCharacterTyped(valueInputBinaryNumber);
+  const valueDecimalNumber = convertToDecimalNumber(valueInputBinaryNumber);
 
   const regex = /([2-9])/;
-  const invalidNumberBinary = regex.test(valueInputBinaryNumber);
+  const isAnInvalidBinaryNumber = validateValueEntry(
+    valueInputBinaryNumber,
+    regex,
+  );
 
-  if (invalidNumberBinary) {
+  if (isAnInvalidBinaryNumber) {
     inputBinaryNumber.classList.add('is-invalid');
     invalidNumber.innerHTML = lastDigitEntered;
-    inputDecimalNumber.value = '';
+    inputDecimalNumber.value = 0;
     return;
   }
 
   inputBinaryNumber.classList.remove('is-invalid');
-  inputDecimalNumber.value = valueDecimalNumber;
-});
+  inputDecimalNumber.value = valueDecimalNumber || 0;
+};
+
+inputBinaryNumber.addEventListener('input', processBinaryToDecimal);
