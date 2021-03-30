1e32091835a9d36d6870dab429caeba9001ebe76
Geisson Iuri <geissoniuri@gmail.com>
2021-03-30 06:50:42 -0300

    codificando

--- projetos/projetos_iniciantes/border_radius_previewer/app_border_radius_previewer.js
+++ projetos/projetos_iniciantes/border_radius_previewer/app_border_radius_previewer.js
@@ -1,2 +1,39 @@
-const rangedTopLeftRx = document.querySelector('#top-left-rx');
-console.log(rangedTopLeftRx.value);
+const inputTopLeftRx = document.querySelector('#top-left-rx');
+const inputTopLeftRy = document.querySelector('#top-left-ry');
+const inputTopRightRx = document.querySelector('#top-right-rx');
+const inputTopRightRy = document.querySelector('#top-right-ry');
+const inputBottomLeftRx = document.querySelector('#bottom-left-rx');
+const inputBottomLeftRy = document.querySelector('#bottom-left-ry');
+const inputBottomRightRx = document.querySelector('#bottom-right-rx');
+const inputBottomRightRy = document.querySelector('#bottom-right-ry');
+const boxBorderRadius = document.querySelector('[data-js=box-border-radius]');
+const containerBoxBorderRadius = document.querySelector(
+  '[data-js=container-box-border-radius]',
+);
+
+console.log(containerBoxBorderRadius);
+console.log(boxBorderRadius);
+console.log(inputTopLeftRx);
+/* console.log(containerBorderRadius);
+console.log(inputTopLeftRx);
+console.log(inputTopLeftRy);
+console.log(inputTopRightRx);
+console.log(inputTopRightRy);
+console.log(inputBottomLeftRx);
+console.log(inputBottomLeftRy);
+console.log(inputBottomRightRx);
+console.log(inputBottomRightRy);
+ */
+/* inputTopLeftRx.addEventListener('input', (event) => {
+  const borderRadiusValue = event.target.value;
+  containerBorderRadius.style.borderTopLeftRadius = `${borderRadiusValue}%`;
+  const codeCss = `
+      <div>
+        -webkit-border-top-left-radius: ${borderRadiusValue}%;
+        -moz-border-radius-topleft: ${borderRadiusValue}%;
+        border-top-left-radius: ${borderRadiusValue}%;
+      </div>
+      `;
+
+  containerBorderRadius.append(codeCss);
+}); */
